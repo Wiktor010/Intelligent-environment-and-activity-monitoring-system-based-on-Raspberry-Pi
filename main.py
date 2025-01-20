cc24108187d7d91fb277ab21d9dc7574ad960e44
Michal <michal.suchta@solarboat.agh.edu.pl>
2025-01-20 17:23:41 +0100

    main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from scripts.MicrophoneRecorder import MicrophoneRecorder
 from scripts.GUI import SensorApp
 
 def run_gui():
+    root = tk.Tk()
     app = SensorApp(root)
     root.protocol("WM_DELETE_WINDOW", lambda: (app.camera_display.stop() if app.camera_display else None, root.destroy()))
     root.mainloop()
@@ -16,7 +17,8 @@ if __name__ == "__main__":
     # Inicjalizacja komponentów
     sensors = Sensors()
     database_handler = SensorDataHandler()
-    
+    variables = Globals()
+
     # Tworzenie i uruchomienie wątku dla GUI
     gui_thread = threading.Thread(target=run_gui, daemon=True)
     gui_thread.start()
@@ -24,6 +26,7 @@ if __name__ == "__main__":
     # Pętla główna działająca w wątku głównym
     while True:
         sensors.read_sensors_data()
+        database_handler.update_sensor_data('test')
         database_handler.insert_sensor_data('test')
         time.sleep(1000)  # Opcjonalny timeout, aby ograniczyć obciążenie CPU
 
