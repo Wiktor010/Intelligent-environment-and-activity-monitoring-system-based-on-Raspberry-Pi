ea2464ae3f0b1de990425e05d34a49147d36ec68
Michal <michal.suchta@solarboat.agh.edu.pl>
2025-01-23 19:39:16 +0100

    adding queue in main

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import threading
 import time
 import tkinter as tk
+from queue import Queue
 from scripts.sensors_handling import Sensors
 from scripts.globals import Globals
 from scripts.sql import SensorDataHandler
 from scripts.MicrophoneRecorder import MicrophoneRecorder
 from scripts.GUI import SensorApp
 
+# Kolejka do komunikacji między wątkami
+gui_queue = Queue()
+
 def run_gui():
+    global app
     root = tk.Tk()
     app = SensorApp(root)
+    gui_queue.put(gui_app)  # Przekazanie instancji do kolejki
     root.protocol("WM_DELETE_WINDOW", lambda: (app.camera_display.stop() if app.camera_display else None, root.destroy()))
     root.mainloop()
 
@@ -23,10 +29,20 @@ if __name__ == "__main__":
     gui_thread = threading.Thread(target=run_gui, daemon=True)
     gui_thread.start()
 
-    # Pętla główna działająca w wątku głównym
-    while True:
-        sensors.read_sensors_data()
-        database_handler.update_sensor_data()
-        database_handler.insert_sensor_data('test')
-        time.sleep(2)  # Opcjonalny timeout, aby ograniczyć obciążenie CPU
+    # Czekaj na inicjalizację GUI
+    while gui_queue.empty():
+        time.sleep(0.1)
+    gui_app = gui_queue.get()  # Pobierz instancję GUI z kolejki
 
+    # Pętla główna działająca w wątku głównym
+    try:
+        while True:
+            sensors.read_sensors_data()
+            database_handler.update_sensor_data()
+            database_handler.insert_sensor_data('test')
+            time.sleep(2)  # Opcjonalny timeout, aby ograniczyć obciążenie CPU
+
+            # Wywołaj funkcję aktualizacji wykresów w GUI w wątku głównym
+            gui_app.root.after(0, gui_app.xxx)  # Metoda `after` wywołuje funkcję w wątku GUI
+    except KeyboardInterrupt as e:
+        print(f"Zamykanie aplikacji z powodu: {e}")
